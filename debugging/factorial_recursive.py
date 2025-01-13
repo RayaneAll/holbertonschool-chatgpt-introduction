1cf7f5f9d842ada7b80d2fe23c54ed7479cded96
RayaneAll <9975@holbertonstudents.com>
2025-01-13 12:06:10 +0100

    Change code factorial_recursive.py

--- debugging/factorial_recursive.py
+++ debugging/factorial_recursive.py
@@ -1,19 +1,11 @@
+#!/usr/bin/python3
 import sys
 
 def factorial(n):
-    if n == 0 or n == 1:
+    if n == 0:
         return 1
     else:
-        return n * factorial(n - 1)
+        return n * factorial(n-1)
 
-# Vérification que l'argument a été passé
-if len(sys.argv) < 2:
-    print("Erreur : Tu dois fournir un argument pour calculer le factoriel.")
-else:
-    try:
-        # Convertir l'argument en entier
-        number = int(sys.argv[1])
-        result = factorial(number)
-        print(f"Le factoriel de {number} est {result}.")
-    except ValueError:
-        print("Erreur : L'argument doit être un entier valide.")
+f = factorial(int(sys.argv[1]))
+print(f)
